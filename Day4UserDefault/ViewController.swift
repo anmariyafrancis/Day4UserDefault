68fd24ca5aba3107eb8df3081edc30fca539bf4f
anmariyafrancis <anmariya1915@gmail.com>
2020-03-04 19:09:42 -0500

    remove button

--- Day4UserDefault/ViewController.swift
+++ Day4UserDefault/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        txtName.text=""
+        lblName.text=""
+    }
 
     @IBAction func btnSaveClick(_ sender: UIButton)
     {
